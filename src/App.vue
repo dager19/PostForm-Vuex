64b0aab86d80bb36f6b33896794a132f6f969eb3
Kirill Lee <keizo.lee@mail.ru>
2020-07-21 08:44:56 +0600

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostForm />
+    <h1>{{postsCount}}</h1>
+    <div class="post" v-for="post in validPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapGetters, mapActions} from 'vuex';
+import PostForm from './components/PostForm'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    computed: mapGetters(["validPosts", "postsCount"]),
+    methods: mapActions(["fetchPosts"]),
+    components: { PostForm },
+    async mounted() {
+        // this.$store.dispatch("fetchPosts");
+        this.fetchPosts();
+    }
+};
 </script>
 
 <style>
@@ -23,6 +32,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
 }
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+
 </style>
---
+++ src/store/modules/post.js
@@ -0,0 +1,39 @@
+export default {
+    actions: {
+        async fetchPosts({commit, dispatch}, limit = 3) {
+            const res = await fetch(
+                'https://jsonplaceholder.typicode.com/posts?_limit=' + limit
+            )
+            const posts = await res.json()
+
+            dispatch('sayHello')
+
+            commit('updatePosts', posts)
+        },
+        sayHello() {}
+    },
+    mutations: {
+        updatePosts(state, posts) {
+            state.posts = posts
+        },
+        createPost(state, newPost) {
+            state.posts.unshift(newPost)
+        }
+    },
+    state: {
+        posts: []
+    },
+    getters: {
+        validPosts(state) {
+          return state.posts.filter(p => {
+              return p.title && p.body
+          })
+        },
+        allPosts(state) {
+            return state.posts
+        },
+        postsCount(state, getters) {
+            return getters.validPosts.length
+        }
+    }
+}
